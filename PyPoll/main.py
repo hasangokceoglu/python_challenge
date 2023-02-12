488c5d77206ce8f0624e0d24738d3ca4d33acd6e
Hasan Gokceoglu <hasangokceoglu07@gmail.com>
2023-02-12 22:27:22 +0000

    final results available. print to txt missing

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,69 @@
 import os
 import csv
+import pandas as pd
 
-resource_path = os.path.join('/Users/hasan/Data Analytics/Projects/Modules/Week3/Week 3 Challenge/Instructions/PyBank/Resources')
+
+from google.colab import files
+files.upload()
+count_row=0
+candidate=[]
+poll_list=[]
+
+vote_counter=-1
+
+Charles_count= 0
+Diana_count= 0
+Raymon_count= 0
+
+Charles_percent= 0
+Diana_percent= 0
+Raymon_percent= 0
+
+
+with open('election_data.csv') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is row header)
+    csv_header = next(csvreader)
+
+    # Count each row of data after the header
+    for row in csvreader:
+      count_row = count_row + 1 
+      vote_counter += 1 
+      # count of polls
+  
+
+      if row[2] == "Charles Casper Stockham":
+            Charles_count += 1
+      elif row[2] == "Diana DeGette":
+            Diana_count += 1
+      elif row[2] == "Raymon Anthony Doane":
+            Raymon_count += 1
+        
+    results = {"Charles Casper Stockham":Charles_count, "Diana DeGette":Diana_count, "Raymon Anthony Doane":Raymon_count}
+
+# percentage calculation
+
+    Charles_percent = round((Charles_count / vote_counter) * 100, 3)
+    Diana_percent = round((Diana_count / vote_counter) * 100, 3)
+    Raymon_percent = round((Raymon_count / vote_counter) * 100, 3)
+
+    # find winner
+    winner = max(results, key=results.get)
+      
+
+
+
+
+       #print results
+    print("Election Results")
+    print("---------------------")
+    print(f"Total Votes:", (count_row))
+    print("---------------------")
+    print(f"Charles Casper Stockham: %", (Charles_percent) ,(Charles_count) )
+    print(f"Diana DeGette: %", (Diana_percent) ,(Diana_count) )
+    print(f"Raymon Anthony Doane: %", (Raymon_percent) ,(Raymon_count) )
+    print("---------------------")
+    print(winner)
